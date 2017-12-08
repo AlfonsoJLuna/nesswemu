be6b5e9a08b9e11abaef058d9f3b76f2def4f3df
AlfonsoJLuna <alfonsoj23l@gmail.com>
2017-12-08 20:03:14 +0100

    Fix Graphics.c building errors.

---
+++ Core/Graphics/Graphics.c
@@ -0,0 +1,42 @@
+#include "Graphics.h"
+
+
+uint64_t Graphics_PPU_Next_VBlank_Cycle()
+{
+    return 0;
+}
+
+void Graphics_PPU_Reset()
+{
+
+}
+
+void Graphics_PPU_Power_Cycle()
+{
+
+}
+
+uint32_t* Graphics_Get_Frame()
+{
+    return NULL;
+}
+
+uint8_t Graphics_PPU_Register_Read(uint16_t Address)
+{
+    return 0;
+}
+
+void Graphics_PPU_Register_Write(uint16_t Address, uint8_t Data)
+{
+
+}
+
+uint8_t Graphics_PPU_Palette_Read(uint16_t Address)
+{
+    return 0;
+}
+
+void Graphics_PPU_Palette_Write(uint16_t Address, uint8_t Data)
+{
+
+}
